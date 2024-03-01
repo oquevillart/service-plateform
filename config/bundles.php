82bd283ffa5d6089ded39b8bb58eb580e1fad008
oquevillart <105644850+oquevillart@users.noreply.github.com>
2024-03-01 16:12:22 +0000

    remove easyadmin

--- config/bundles.php
+++ config/bundles.php
@@ -9,6 +9,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
